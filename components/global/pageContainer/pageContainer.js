ad21e7a075e5758b177ebd0392cb8749c1d54d0d
Sam Foltin <sam.foltin@gmail.com>
2022-06-15 20:39:21 +0100

    Add ability to set page background color via PageContainer component

--- components/global/pageContainer/pageContainer.js
+++ components/global/pageContainer/pageContainer.js
@@ -2,10 +2,10 @@
 import NavigationBar from "./NavigationBar"
 import Footer from "./footer"
 
-export default function PageContainer({ children }) {
+export default function PageContainer({ children, color }) {
     return(
         <div className='flex flex-col justify-between w-screen min-h-screen'>
-            <div>
+            <div style={{ backgroundColor: color }}>
                 <NavigationBar />
                 { children }
             </div>
