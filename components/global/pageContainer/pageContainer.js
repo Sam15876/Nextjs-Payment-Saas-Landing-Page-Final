d874056571d2cd90ef9d69b4d4eb1ba22d6a0c98
Sam Foltin <sam.foltin@gmail.com>
2022-06-15 16:25:18 +0100

    Fix issue with nextjs failing to hydrate ui

--- components/global/pageContainer/pageContainer.js
+++ components/global/pageContainer/pageContainer.js
@@ -4,12 +4,12 @@ import Footer from "./footer"
 
 export default function PageContainer({ children }) {
     return(
-        <body className='flex flex-col justify-between w-screen min-h-screen'>
+        <div className='flex flex-col justify-between w-screen min-h-screen'>
             <div>
                 <NavigationBar />
                 { children }
             </div>
             <Footer />
-        </body>
+        </div>
     )
 }
