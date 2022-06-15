75b3078834c05324db227a6d9aecd72b6b086708
Sam Foltin <sam.foltin@gmail.com>
2022-06-15 18:44:35 +0100

    Add heading to howItWorks page

---
+++ components/howItWorks/heading.js
@@ -0,0 +1,10 @@
+export default function Heading() {
+    return(
+        <div className='w-full flex justify-center mt-44'>
+            <header className='max-w-lg flex flex-col items-center'>
+                <h1 className='text-7xl font-bold'>How it works</h1>
+                <p className='text-xl opacity-70 text-center py-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.</p>
+            </header>
+        </div>
+    )
+}
--- pages/howItWorks/index.js
+++ pages/howItWorks/index.js
@@ -1,9 +1,11 @@
 // Components
 import PageContainer from "../../components/global/pageContainer/pageContainer"
+import Heading from "../../components/howItWorks/heading"
 
 export default function HowItWorks() {
     return(
         <PageContainer>
+            <Heading />
         </PageContainer>
     )
 }
